b0fb75c5973cc3eb4d1b55ff4251fad32ceadb42
Peter Forbes <git@peteyjoe.com>
2026-08-08 16:34:45 -0700

    added vaultwarden config

---
+++ modules/services/vaultwarden.nix
@@ -0,0 +1,32 @@
+{ config, ... }:
+
+{
+  sops.secrets.vaultwarden-admin-token = {
+    sopsFile = ../../secrets/the-box.yaml;
+  };
+
+  sops.templates."vaultwarden.env" = {
+    content = ''
+      ADMIN_TOKEN=${config.sops.placeholder.vaultwarden-admin-token}
+    '';
+    owner = "vaultwarden";
+  };
+
+  services.vaultwarden = {
+    enable = true;
+
+    environmentFile =
+      config.sops.templates."vaultwarden.env".path;
+
+    config = {
+      DOMAIN = "https://vault.thisismy.casa";
+      ROCKET_ADDRESS = "127.0.0.1";
+      ROCKET_PORT = 8222;
+      SIGNUPS_ALLOWED = false;
+    };
+  };
+
+  services.caddy.virtualHosts."vault.thisismy.casa".extraConfig = ''
+    reverse_proxy 127.0.0.1:8222
+  '';
+}
