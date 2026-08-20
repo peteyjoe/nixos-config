334331496505b04a4f43e92002ab7d1418a33d96
Peter Forbes <git@peteyjoe.com>
2026-08-20 03:03:08 -0700

    update wayfire config

--- home/peteyjoe/wayfire.nix
+++ home/peteyjoe/wayfire.nix
@@ -9,5 +9,53 @@
     wf-shell.enable = true;
     xwayland.enable = true;
 
+    settings = {
+      core = {
+        plugins = ''
+          alpha
+          animate
+          command
+          cube
+          decoration
+          expo
+          fast-switcher
+          fisheye
+          foreign-toplevel
+          grid
+          gtk-shell
+          idle
+          invert
+          move
+          oswitch
+          place
+          resize
+          session-lock
+          shortcuts-inhibit
+          switcher
+          vswitch
+          window-rules
+          wm-actions
+          wobbly
+          wrot
+          zoom
+        '';
+
+        close_top_view = "<super> KEY_Q | <alt> KEY_F4";
+
+        vwidth = 3;
+        vheight = 3;
+      };
+
+      command = {
+        binding_terminal = "<super> KEY_ENTER";
+        command_terminal = "ghostty";
+
+        binding_launcher = "<super> KEY_D";
+        command_launcher = "fuzzel";
+      };
+
+      move.activate = "<super> BTN_LEFT";
+      resize.activate = "<super> BTN_RIGHT";
+    };
   };
 }
