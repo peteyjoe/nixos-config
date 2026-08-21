b36e256d3c76246ca12f294afae6dd7af07596bb
Peter <git@peteyjoe.com>
2026-08-21 02:04:12 +0000

    update monitor config

--- home/peteyjoe/wayfire.nix
+++ home/peteyjoe/wayfire.nix
@@ -10,6 +10,27 @@
     xwayland.enable = true;
 
     settings = {
+      "output:DP-1" = {
+        mode = "1920x1080@74.973";
+        position = "0,0";
+        transform = "90";
+        scale = 1.0;
+      };
+
+      "output:DP-2" = {
+        mode = "2560x1440@240.001007";
+        position = "1080,240";
+        transform = "normal";
+        scale = 1.0;
+      };
+
+      "output:HDMI-A-1" = {
+        mode = "1920x1080@60";
+        position = "3640,0";
+        transform = "270";
+        scale = 1.0;
+      };
+
       core = {
         plugins = builtins.concatStringsSep " " [
           "alpha"
