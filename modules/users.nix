4877aa1710af8f54e1c2062d7168702a23cc4c06
Peter Forbes <git@peteyjoe.com>
2026-08-24 08:44:29 -0700

    add pub ssh key

--- modules/users.nix
+++ modules/users.nix
@@ -12,6 +12,7 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILKSoyzrAENRjP8H3MFeIm2IYU8Z0cyim7r1LJxRZVBm peteyjoe@big-juan"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMGyqWdX6HLecqAzpzwX/waCMN/4nIkfKCiEreSJucsC peteyjoe@the-box"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFayQe38Wnl4I1qFlkSzXE9LYakwVpER9fBQjdkm1Nsk peteyjoe@santiago"
     ];
   };
 
